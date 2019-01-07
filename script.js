40f8a885d8b356975d3c53e865ef0f5061622e1a
bealkl <bealkl@gmail.com>
2019-01-07 17:46:11 +0200

    Task 1

--- script.js
+++ script.js
@@ -1,15 +1,17 @@
 'use strict';
 
-//var leftBorderWidth=1;
-//let second=2;
-//const pi=3.1415926;
+var money;
+var time;
 
-/*let persone={
-    name: "Linka",
-    age: 34,
-    isMarried: true
-};*/
+money=prompt("Your month budget","0");
+time=prompt("Enter data YYYY-MM-DD","2019-01-01");
 
-//console.log(persone.name);
-// alert("Hello world;");
+console.log("Money="+money);
+console.log("Time="+time);
 
+let  appData={budget:money, timeData:time, expenses:0, optionalExpenses:0, income:0, saving:false};
+
+appData.expenses=prompt("Spend in this month","");
+appData.optionalExpenses=prompt("How much?","");
+
+alert("Budget for day="+(appData.budget-appData.expenses-appData.optionalExpenses)/30);
