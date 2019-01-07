dd2003a43b23ca7d9b6beaabca021d745e09293d
bealkl <bealkl@gmail.com>
2019-01-07 15:34:13 +0200

    this is the first commite

---
+++ script.js
@@ -0,0 +1,15 @@
+'use strict';
+
+//var leftBorderWidth=1;
+//let second=2;
+//const pi=3.1415926;
+
+/*let persone={
+    name: "Linka",
+    age: 34,
+    isMarried: true
+};*/
+
+//console.log(persone.name);
+// alert("Hello world;");
+
